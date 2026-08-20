aa8a6f746bcbc3b141b7caadd4d02ffbd62c0ecc
Xanthos Kekkos <xkekkos17@gmail.com>
2026-08-20 17:29:31 +0200

    - Refactor code structure for improved readability and maintainability - Add gtg-euclidean tasks and job file to run them

---
+++ jobs/generate_tasks/gtg_euclidean.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+dims=(2 3 4 8 16 64 256 512)
+seeds=(34 42 50)
+
+dist=0.5
+std_x0=0.3
+std_x1=0.3
+
+output_file="tasks/gtg_euclidean.txt"
+
+for seed in "${seeds[@]}"; do
+  for dim in "${dims[@]}"; do
+
+    read mean_x0 mean_x1 <<< $(python - <<EOF
+import numpy as np
+
+dim = $dim
+dist = $dist
+
+u = np.ones(dim)
+u = u / np.linalg.norm(u)
+
+x0 = np.zeros(dim)
+x1 = dist * u
+
+mean_x0 = "[" + ",".join([f"{v:.8f}" for v in x0]) + "]"
+mean_x1 = "[" + ",".join([f"{v:.8f}" for v in x1]) + "]"
+
+print(mean_x0, mean_x1)
+EOF
+)
+
+    echo "srun python train.py experiment=general_fm seed=${seed} hydra.run.dir=outputs/runs/gtg/euclidean/general/euc_d${dim}_s${seed} general.manifold=euclidean general.dim=${dim} general.curvature=0.0 general.std_x0=${std_x0} general.std_x1=${std_x1} general.mean_x0=${mean_x0} general.mean_x1=${mean_x1} general.normalize_tangent_distributions=False" >> "$output_file"
+  
+  done
+done
+
+echo "Successfully generated $(wc -l < "$output_file") tasks in $output_file"
