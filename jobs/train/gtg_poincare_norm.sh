1659ee6ca3f50c473ffdfeaeff46c9c5b12c8fbe
Xanthos Kekkos <xkekkos17@gmail.com>
2026-08-21 14:51:06 +0200

    Fixed directories.

--- jobs/train/gtg_poincare_norm.sh
+++ jobs/train/gtg_poincare_norm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=gtg_poi_norm
-#SBATCH --time=04:00:00            # Adjust as needed
-#SBATCH --partition=gpu            # Standard Snellius GPU partition
+#SBATCH --time=08:00:00            # Adjust as needed
+#SBATCH --partition=gpu_h100       # Standard Snellius GPU partition
 #SBATCH --gpus=1                   # GPUs per task
 #SBATCH --cpus-per-task=9          # Standard CPU allocation for 1 GPU on Snellius
 #SBATCH --array=1-120               
@@ -9,7 +9,7 @@
 #SBATCH --error=logs/gtg/poincare/norm_array_%A_%a.err  
 
 # Create necessary directories
-mkdir -p logs gtg poincare failed_tasks
+mkdir -p logs/gtg/poincare output/gtg/poincare failed_tasks/gtg/poincare
 
 TASKS_FILE="tasks/gtg_poincare_norm.txt"
 
@@ -29,8 +29,8 @@ if [ $EXIT_CODE -ne 0 ]; then
     echo "Task $SLURM_ARRAY_TASK_ID failed with exit code $EXIT_CODE."
     
     # Copy both standard output and error files to the failed_tasks directory
-    cp "logs/array_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}_norm.out" failed_tasks/
-    cp "logs/array_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}_norm.err" failed_tasks/
+    cp "output/gtg/poincare/norm_array_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.out" failed_tasks/gtg/poincare/
+    cp "logs/gtg/poincare/norm_array_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.err" failed_tasks/gtg/poincare/
     
     echo "Logs copied to failed_tasks/ directory."
 fi
